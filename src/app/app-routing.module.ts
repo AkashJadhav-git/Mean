c4c382a9bd3e004726bae0df9bd05dd88fbdaaa2
Akash Jadhav <55876320+AkashJadhav-git@users.noreply.github.com>
2020-02-04 14:50:35 +0530

    show product component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : '',
+    pathMatch : 'full',
+    component : HomeComponent
+  },
+  {
+  path : 'jobs',
+  pathMatch : 'full',
+  loadChildren : './jobs/jobs.module#JobsModule'
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { VpMaterialModule } from './material-module';
 import { HomeComponent } from './home/home.component';
+import { JobsModule } from './jobs/jobs.module';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { HomeComponent } from './home/home.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    VpMaterialModule
+    VpMaterialModule,
+    JobsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
