23afd389299f2db189c3870befcefdf84ed5b62d
Akash Jadhav <55876320+AkashJadhav-git@users.noreply.github.com>
2020-02-04 12:23:35 +0530

    home component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { VpMaterialModule } from './material-module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
