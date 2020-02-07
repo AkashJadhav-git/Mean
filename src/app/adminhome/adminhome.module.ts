927caf4cdcce0358c811418ad5e80002dbb96543
Akash Jadhav <55876320+AkashJadhav-git@users.noreply.github.com>
2020-02-07 17:54:39 +0530

    create pietree

--- src/app/adminhome/adminhome.module.ts
+++ src/app/adminhome/adminhome.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { AdminhomeRoutingModule } from './adminhome-routing.module';
+import {AccumulationChartModule, PieSeriesService, AccumulationTooltipService, AccumulationDataLabelService} from '@syncfusion/ej2-angular-charts';
+import { AdminhomeRoutingModule} from './adminhome-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 
@@ -9,7 +9,11 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   declarations: [DashboardComponent],
   imports: [
     CommonModule,
-    AdminhomeRoutingModule
-  ]
+    AdminhomeRoutingModule,
+    AccumulationChartModule
+  ],
+    bootstrap : [DashboardComponent],
+    providers : [PieSeriesService, AccumulationDataLabelService, AccumulationTooltipService]
+
 })
 export class AdminhomeModule { }
--- src/app/adminhome/dashboard/dashboard.component.ts
+++ src/app/adminhome/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { inside } from '@syncfusion/ej2-angular-charts';
+import { MAT_BOTTOM_SHEET_DATA } from '@angular/material/bottom-sheet';
+
 
 @Component({
   selector: 'pm-dashboard',
@@ -7,7 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  public data:Object[];
+  public chartTitle : String;
+  public chartLabel : Object;
+  public tooltip : object;
+
+  constructor() { 
+    this.chartTitle = 'Placements Engineering Departmentwise'
+    this.data = [
+      {name : 'Compueter', value : 37, text : '37%'},
+      {name : 'Mechanical', value : 17, text : '17%'},
+      {name : 'Civil', value : 19, text : '19%'},
+      {name : 'Elecrical', value : 4, text : '4%'},
+      {name : 'EXTC', value : 11, text : '11%'},
+      {name : 'Others', value : 12, text : '12%'}
+
+    ];
+    this.chartLabel = {
+      visible : true,
+      position : 'Inside',
+      name : 'text'
+    }
+    this.tooltip = {
+     enable : true,
+     format : '${point.x} : <b>${point.y}%</b>'
+    }
+  }
 
   ngOnInit() {
   }
