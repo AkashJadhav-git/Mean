0183a70bfe1e32706757bffc7e4375e65565c9ef
Akash Jadhav <55876320+AkashJadhav-git@users.noreply.github.com>
2020-02-08 20:15:46 +0530

    mongoose setting done

--- server/config/config.js
+++ server/config/config.js
@@ -2,5 +2,10 @@ require('dotenv').config(); // pull all data from .env file and put into process
 const envVars = process.env;
 module.exports = {
     port: envVars.PORT,
-    env : envVars.NODE_ENV
+    env : envVars.NODE_ENV,
+    mongo :{
+        uri: envVars.MONGODB_URI,
+        port: envVars.MONGO_PORT,
+        isDebug: envVars.MONGOOSE_DEBUG
+    }
 };
