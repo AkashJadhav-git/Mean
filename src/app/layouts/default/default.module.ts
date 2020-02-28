63fad4993c02406c8f0cc8452a3f844e8c60d05a
Akash Jadhav <55876320+AkashJadhav-git@users.noreply.github.com>
2020-02-28 09:45:15 +0530

    install tslib

--- src/app/layouts/default/default.module.ts
+++ src/app/layouts/default/default.module.ts
@@ -8,6 +8,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 
 
@@ -24,6 +25,8 @@ import { MatMenuModule } from '@angular/material/menu';
     MatButtonModule,
     MatMenuModule,
     MatListModule,
+    FlexLayoutModule,
+
   ],
 
 })
